f375dfc999d27b72ab6a5e0155336ff672fc8026
Szymon Krywus <szymon.krywus@MWPLKRAMACLT012.logthemin.com>
2017-07-19 14:32:10 +0200

    update

--- translation.js
+++ translation.js
@@ -1,18 +1,20 @@
-import {RTM_TOKEN, TARGET_LANGUAGE, PROJECT_ID} from './config';
+import {TOKEN, TARGET_LANGUAGE, PROJECT_ID} from './config';
 
 const Translate = require('@google-cloud/translate');
 const RtmClient = require('@slack/client').RtmClient;
+const WebClient = require('@slack/client').WebClient;
 const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 const translateClient = Translate({
     projectId: PROJECT_ID,
     keyFilename: './slackTranslationBot-bd49c16473e1.json'
 });
 
-const rtm = new RtmClient(RTM_TOKEN);
+const web = new WebClient(TOKEN);
+const rtm = new RtmClient(TOKEN);
 rtm.start();
 
 rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
-console.log("message", message);
+
     translateClient.detect(message.text)
         .then((detection) => {
 
@@ -20,12 +22,14 @@ console.log("message", message);
 
                 translateClient.translate(message.text, TARGET_LANGUAGE)
                     .then((translation) => {
-                        rtm._send({
+                        /*rtm._send({
                             type: 'message',
                             text: "[Translation]:" + translation[0],
                             channel: message.channel,
-                            'as_user': true
-                        });
+                            as_user: true
+                        });*/
+                        console.log("message", message);
+                        rtm.chat.update(message.channel, "lalalal", message.ts)
                     })
                     .catch((err) => {
                         console.error('ERROR:', err);
